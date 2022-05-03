e18d2c42aa1e5bfe45e6c029dfd5a8969ae36972
Zhijie He <hezhijie0327@hotmail.com>
2022-05-03 11:14:23 +0800

    adguardhome.sh | Version 1.0.202205031114
    1. <New> First upload.

---
+++ adguardhome/adguardhome.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Parameter
+OWNER="hezhijie0327"
+REPO="adguardhome"
+TAG="latest"
+DOCKER_PATH="/docker/adguardhome"
+
+## Function
+# Get Latest Image
+function GetLatestImage() {
+    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
+}
+# Cleanup Current Container
+function CleanupCurrentContainer() {
+    if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^${REPO}$") ]; then
+        docker stop ${REPO} && docker rm ${REPO}
+    fi
+}
+# Create New Container
+function CreateNewContainer() {
+    docker run --name ${REPO} --net host --restart=always \
+        -v ${DOCKER_PATH}/cert:/etc/adguardhome/cert:ro \
+        -v ${DOCKER_PATH}/conf:/etc/adguardhome/conf \
+        -v ${DOCKER_PATH}/work:/etc/adguardhome/work \
+        -d ${OWNER}/${REPO}:${TAG} \
+        --config "/etc/adguardhome/conf/AdGuardHome.yaml" \
+        --work-dir "/etc/adguardhome/work" \
+        --no-check-update
+}
+# Cleanup Expired Image
+function CleanupExpiredImage() {
+    if [ "${IMAGES}" != "" ]; then
+        docker rmi ${IMAGES}
+    fi
+}
+
+## Process
+# Call GetLatestImage
+GetLatestImage
+# Call CleanupCurrentContainer
+CleanupCurrentContainer
+# Call CreateNewContainer
+CreateNewContainer
+# Call CleanupExpiredImage
+CleanupExpiredImage
