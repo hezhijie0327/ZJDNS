f0dc7569dfafa1bcebb706562e44d373d178f3cd
Zhijie He <hezhijie0327@hotmail.com>
2022-05-10 10:00:05 +0800

    gfw_polluted_dns.sh | Version 1.0.202205100959
    1. <Change> Fix an error.

--- mosdns/gfw_polluted_dns.sh
+++ mosdns/gfw_polluted_dns.sh
@@ -6,7 +6,7 @@ TOTAL_TIME="300"
 
 rm -rf ./gfw_polluted_ipv*.txt
 
-for (( i = 1; i <= ${TOTAL_TIME}; i++)); do
+for (( i = 1; i <= ${TOTAL_TIME}; i++ )); do
     echo "$(dig @${FAKE_DNS} ${GFW_DOMAIN} +short A | cut -d "." -f 1-3).0/24" >> "./gfw_polluted_ipv4.tmp"
     echo "$(dig @${FAKE_DNS} ${GFW_DOMAIN} +short AAAA | cut -d ":" -f 1-3):0/112" >> "./gfw_polluted_ipv6.tmp"
 done
