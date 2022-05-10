2483e3c3328f43f03f34154b6853540ad110efcd
Zhijie He <hezhijie0327@hotmail.com>
2022-05-10 13:28:14 +0800

    gfw_polluted_dns.sh | Version 1.0.202205101327
    1. <Change> Auto combine the current data from online.

--- mosdns/gfw_polluted_dns.sh
+++ mosdns/gfw_polluted_dns.sh
@@ -2,9 +2,13 @@
 
 GFW_DOMAIN="zh.wikipedia.org"
 FAKE_DNS="11.45.1.4"
+GHPROXY_URL="https://ghproxy.com/"
 TOTAL_TIME="300"
 
 rm -rf ./gfw_polluted_ipv*.txt
+
+curl -s --connect-timeout 15 "${GHPROXY_URL}https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/gfw_polluted_ipv4.txt" > "./gfw_polluted_ipv4.tmp"
+curl -s --connect-timeout 15 "${GHPROXY_URL}https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/gfw_polluted_ipv6.txt" > "./gfw_polluted_ipv6.tmp"
 
 for (( i = 1; i <= ${TOTAL_TIME}; i++ )); do
     echo "$(dig @${FAKE_DNS} ${GFW_DOMAIN} +short A | cut -d "." -f 1-3).0/24" >> "./gfw_polluted_ipv4.tmp"
