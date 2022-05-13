671b265520bd7a9752800cbe17dc8c9893175ba3
Zhijie He <hezhijie0327@hotmail.com>
2022-05-13 13:18:32 +0800

    mosdns.sh | Version 1.0.202205131318

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -10,13 +10,10 @@ GHPROXY_URL="https://ghproxy.com/"
 ## Function
 # Get Latest Image
 function GetLatestImage() {
-    docker pull redis:latest && docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
+    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
 }
 # Cleanup Current Container
 function CleanupCurrentContainer() {
-    if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^redis$") ]; then
-        docker stop redis && docker rm redis
-    fi
     if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^${REPO}$") ]; then
         docker stop ${REPO} && docker rm ${REPO}
     fi
@@ -57,12 +54,6 @@ function UpdateGFWPollutedIPRule() {
 }
 # Create New Container
 function CreateNewContainer() {
-    docker run --name redis --net host --restart=always \
-        -v ${DOCKER_PATH}/conf/redis.conf:/etc/redis/redis.conf \
-        -v ${DOCKER_PATH}/data:/data \
-        -d redis:latest \
-        redis-server /etc/redis/redis.conf \
-        --appendonly yes
     docker run --name ${REPO} --net host --restart=always \
         -v /docker/ssl:/etc/mosdns/cert:ro \
         -v ${DOCKER_PATH}/conf:/etc/mosdns/conf \
