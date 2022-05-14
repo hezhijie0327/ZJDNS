5203385ee91bd71ea38076a22a26dc7e043298ff
Zhijie He <hezhijie0327@hotmail.com>
2022-05-14 23:19:51 +0800

    GeneratePollutedIP.sh | Version 1.0.202205142319

--- mosdns/GeneratePollutedIP.sh
+++ mosdns/GeneratePollutedIP.sh
@@ -5,14 +5,14 @@ FAKE_DNS="11.45.1.4"
 GHPROXY_URL="https://ghproxy.com/"
 TOTAL_TIME="300"
 
-rm -rf "./POLLUTED.tmp" "./POLLUTED.txt"
+rm -rf "./GFW_Polluted_IP.tmp" "./GFW_Polluted_IP.txt"
 
-curl -s --connect-timeout 15 "${GHPROXY_URL}https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/POLLUTED.txt" > "./POLLUTED.tmp"
+curl -s --connect-timeout 15 "${GHPROXY_URL}https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/GFW_Polluted_IP.txt" > "./GFW_Polluted_IP.tmp"
 
 for (( i = 1; i <= ${TOTAL_TIME}; i++ )); do
-    echo "$(dig @${FAKE_DNS} ${GFW_DOMAIN} +short A | cut -d "." -f 1-3).0/24" >> "./POLLUTED.tmp"
+    echo "$(dig @${FAKE_DNS} ${GFW_DOMAIN} +short A | cut -d "." -f 1-3).0/24" >> "./GFW_Polluted_IP.tmp"
 done
 
-cat "./POLLUTED.tmp"| sort | uniq > "./POLLUTED.txt"
+cat "./GFW_Polluted_IP.tmp"| sort | uniq > "./GFW_Polluted_IP.txt"
 
-rm -rf "./POLLUTED.tmp"
+rm -rf "./GFW_Polluted_IP.tmp"
