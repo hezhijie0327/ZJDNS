68d32ca9a67a4dcb9b1d7ac7c1de630af1f34ec9
Zhijie He <hezhijie0327@hotmail.com>
2022-05-26 20:28:50 +0800

    mosdns.sh | Version 1.0.202205262028
    1. <Change> remove limit.

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -41,7 +41,7 @@ function CreateNewContainer() {
         -v ${DOCKER_PATH}/data:/data \
         -d redis:latest \
         --appendonly yes \
-        --maxmemory 64m \
+        --maxmemory 0 \
         --maxmemory-policy allkeys-lru \
         --maxmemory-samples 10
     docker run --name ${REPO} --net host --restart=always \
