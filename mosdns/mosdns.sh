4f522f92af47938beb43a8ff684f6cb3d3c4eae7
Zhijie He <hezhijie0327@hotmail.com>
2022-06-05 06:52:46 +0800

    mosdns.sh | Version 1.0.202206050652
    1. <Change> Add cache back.

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -9,10 +9,13 @@ DOCKER_PATH="/docker/mosdns"
 ## Function
 # Get Latest Image
 function GetLatestImage() {
-    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
+    docker pull redis:latest && docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
 }
 # Cleanup Current Container
 function CleanupCurrentContainer() {
+    if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^redis$") ]; then
+        docker stop redis && docker rm redis
+    fi
     if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^${REPO}$") ]; then
         docker stop ${REPO} && docker rm ${REPO}
     fi
@@ -23,6 +26,13 @@ function UpdateGeoIPCNRule() {
 }
 # Create New Container
 function CreateNewContainer() {
+    docker run --name redis --net host --restart=always \
+        -v ${DOCKER_PATH}/data:/data \
+        -d redis:latest \
+        --appendonly yes \
+        --maxmemory 64m \
+        --maxmemory-policy allkeys-lru \
+        --maxmemory-samples 10
     docker run --name ${REPO} --net host --restart=always \
         -v /docker/ssl:/etc/mosdns/cert:ro \
         -v ${DOCKER_PATH}/conf:/etc/mosdns/conf \
