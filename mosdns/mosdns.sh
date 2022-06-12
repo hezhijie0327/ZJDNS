1b0c1e3c5aa2ff2ddb3313cc9f2317ce1a6a27ec
Zhijie He <hezhijie0327@hotmail.com>
2022-06-12 10:05:38 +0800

    mosdns.sh | Version 1.0.202206121005
    1. <Change> Fix maxmemory error. (Real 64M)

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -39,7 +39,7 @@ function CreateNewContainer() {
         --lazyfree-lazy-eviction yes \
         --lazyfree-lazy-expire yes \
         --lazyfree-lazy-server-del yes \
-        --maxmemory 64m \
+        --maxmemory 64MB \
         --maxmemory-policy allkeys-lru \
         --maxmemory-samples 10
     docker run --name ${REPO} --net host --restart=always \
