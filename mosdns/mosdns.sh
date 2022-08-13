131bee0af57a425b6a3a2df66040b2bcefeb043b
Zhijie He <hezhijie0327@hotmail.com>
2022-08-13 11:01:20 +0800

    mosdns.sh | Version 1.0.202208131100
    1. <Change> Add ssl support.

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -19,7 +19,21 @@ function CleanupCurrentContainer() {
 }
 # Download mosDNS Configuration
 function DownloadmosDNSConfiguration() {
+    ENABLE_SSL="false"
+    SSL_CONFIG=(
+        "  - exec: secure_server"
+        "    listeners:"
+        "      - protocol: tls"
+        "        addr: ':8853'"
+        "        cert: '/etc/adguardhome/cert/fullchain.cer'"
+        "        key: '/etc/adguardhome/cert/zhijie.online.key'"
+    )
     curl -s --connect-timeout 15 "https://source.zhijie.online/CMA_DNS/main/mosdns/config.yaml" > "${DOCKER_PATH}/conf/config.yaml"
+    if [ "${ENABLE_SSL}" == "true" ]; then
+        for SSL_CONFIG_TASK in "${!SSL_CONFIG[@]}"; do
+            echo "${SSL_CONFIG[$SSL_CONFIG_TASK]}" >> "${DOCKER_PATH}/conf/config.yaml"
+        done
+    fi
 }
 # Update GeoIP CN Rule
 function UpdateGeoIPCNRule() {
@@ -29,6 +43,7 @@ function UpdateGeoIPCNRule() {
 # Create New Container
 function CreateNewContainer() {
     docker run --name ${REPO} --net host --restart=always \
+        -v /docker/ssl:/etc/adguardhome/cert:ro \
         -v ${DOCKER_PATH}/conf:/etc/mosdns/conf \
         -v ${DOCKER_PATH}/data:/etc/mosdns/data \
         -d ${OWNER}/${REPO}:${TAG} \
