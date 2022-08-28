a506f9bb96869db7038c3bfd0e6fb394812489ca
Zhijie He <hezhijie0327@hotmail.com>
2022-08-28 20:50:24 +0800

    unbound.sh | Version 1.0.202208282050
    1. <New> First upload.

---
+++ unbound/unbound.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Parameter
+OWNER="hezhijie0327"
+REPO="unbound"
+TAG="latest"
+DOCKER_PATH="/docker/unbound"
+
+## Function
+# Get Latest Image
+function GetLatestImage() {
+    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
+}
+# Cleanup Current Container
+function CleanupCurrentContainer() {
+    if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^${REPO}$") ]; then
+        docker stop ${REPO} && docker rm ${REPO}
+    fi
+}
+# Update Root Hints
+function UpdateRootHints() {
+    curl -s --connect-timeout 15 "https://www.internic.net/domain/named.cache" > "${DOCKER_PATH}/root.hints"
+}
+# Create New Container
+function CreateNewContainer() {
+    docker run -it --rm --entrypoint=/unbound-anchor \
+        -v ${DOCKER_PATH}:/usr/local/etc/unbound \
+        ${OWNER}/${REPO}:${TAG}
+    docker run --name ${REPO} --net host --restart=always \
+        -v /docker/ssl:/usr/local/etc/ssl:ro \
+        -v ${DOCKER_PATH}:/usr/local/etc/unbound \
+        -d ${OWNER}/${REPO}:${TAG} \
+        -c "/usr/local/etc/unbound/unbound.conf" \
+        -d
+}
+# Cleanup Expired Image
+function CleanupExpiredImage() {
+    if [ "${IMAGES}" != "" ]; then
+        docker rmi ${IMAGES}
+    fi
+}
+
+## Process
+# Call GetLatestImage
+GetLatestImage
+# Call CleanupCurrentContainer
+CleanupCurrentContainer
+# Call UpdateRootHints
+UpdateRootHints
+# Call CreateNewContainer
+CreateNewContainer
+# Call CleanupExpiredImage
+CleanupExpiredImage
