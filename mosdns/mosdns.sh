5d9e03de8df05dc06d5d4a24f360181839235418
Zhijie He <hezhijie0327@hotmail.com>
2022-11-16 21:16:04 +0800

    mosdns.sh | Version 1.0.202211162115
    1. <Change> Fix an error.

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -55,7 +55,7 @@ function DownloadmosDNSConfiguration() {
         sed -i "s/\#\@/\ \ /g" "${DOCKER_PATH}/conf/config.yaml"
     fi
     if [ "${ENABLE_REDIS_CACHE}" == "true" ]; then
-        sed -i "s/\#\*/\ \ /g;s/\ \ \ \ \ \ size/\#\*\ \ \ \ size/g" "${DOCKER_PATH}/conf/config.yaml"
+        sed -i "s/\#\*/\ \ /g;s/\ \ \ \ \ \ size/\#\*\ \ \ \ size/g;s/\#\*\ \ \ \ size\:\ 1232/\ \ \ \ \ \ size\:\ 1232/g" "${DOCKER_PATH}/conf/config.yaml"
     fi
     if [ "${ENABLE_HTTPS}" == "true" ]; then
         for HTTPS_CONFIG_TASK in "${!HTTPS_CONFIG[@]}"; do
