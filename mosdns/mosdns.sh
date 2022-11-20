b7361a9eb2a03f86f18e222f976ac5c8c933abb2
Zhijie He <hezhijie0327@hotmail.com>
2022-11-20 20:19:40 +0800

    mosdns.sh | Version 1.0.202211202019

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -60,7 +60,7 @@ function DownloadmosDNSConfiguration() {
         sed -i "s/\#\@/\ \ /g" "${DOCKER_PATH}/conf/config.yaml"
     fi
     if [ "${ENABLE_REDIS_CACHE}" == "true" ]; then
-        sed -i "s/\#\*/\ \ /g;s/\ \ \ \ \ \ size/\#\*\ \ \ \ size/g;s/\#\*\ \ \ \ size\:\ 1232/\ \ \ \ \ \ size\:\ 1232/g" "${DOCKER_PATH}/conf/config.yaml"
+        sed -i "s/\#\*/\ \ /g;s/\ \ \ \ \ \ size/\#\*\ \ \ \ size/g" "${DOCKER_PATH}/conf/config.yaml"
     fi
     if [ "${ENABLE_UNENCRYPTED_DNS}" == "false" ]; then
         if [ "${ENABLE_HTTPS}" == "true" ] || [ "${ENABLE_TLS}" == "true" ]; then
