1d3a0d92c86f54038096d72154ef65fd059d2c95
Zhijie He <hezhijie0327@hotmail.com>
2022-12-06 14:07:51 +0800

    unbound.sh | Version 1.0.202212061407

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -20,22 +20,29 @@ function CleanupCurrentContainer() {
 }
 # Download Unbound Configuration
 function DownloadUnboundConfiguration() {
-    if [ "${USE_CDN}" == true ]; then
-        CDN_PATH="source.zhijie.online"
-    else
-        CDN_PATH="raw.githubusercontent.com/hezhijie0327"
-    fi
+    ENABLE_LOGFILE="false"
+
     ENABLE_HTTPS="false"
     ENABLE_TLS="false"
     SSL_CERT="fullchain.cer"
     SSL_KEY="zhijie.online.key"
+
+    if [ "${USE_CDN}" == true ]; then
+        CDN_PATH="source.zhijie.online"
+    else
+        CDN_PATH="raw.githubusercontent.com/hezhijie0327"
+    fi && curl -s --connect-timeout 15 "https://${CDN_PATH}/CMA_DNS/main/unbound/unbound.conf" | sed "s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" > "${DOCKER_PATH}/data/unbound.conf"
+
+    if [ "${ENABLE_LOGFILE}" == "true" ]; then
+        sed -i "s/##/  /g" "${DOCKER_PATH}/data/unbound.conf"
+    fi
+
     if [ "${ENABLE_HTTPS}" == "true" ]; then
-        SED_ENABLE_HTTPS="s/\#\ \ \ \ interface\:\ 0\.0\.0\.0\@5443/\ \ \ \ interface\:\ 0\.0\.0\.0\@5443/g;s/\#\ \ \ \ interface\:\ \:\:\@5443/\ \ \ \ interface\:\ \:\:\@5443/g;"
+        sed -i "s/#@/  /g" "${DOCKER_PATH}/data/unbound.conf"
     fi
     if [ "${ENABLE_TLS}" == "true" ]; then
-        SED_ENABLE_TLS="s/\#\ \ \ \ interface\:\ 0\.0\.0\.0\@5853/\ \ \ \ interface\:\ 0\.0\.0\.0\@5853/g;s/\#\ \ \ \ interface\:\ \:\:\@5853/\ \ \ \ interface\:\ \:\:\@5853/g;"
+        sed -i "s/#%/  /g" "${DOCKER_PATH}/data/unbound.conf"
     fi
-    curl -s --connect-timeout 15 "https://${CDN_PATH}/CMA_DNS/main/unbound/unbound.conf" | sed "s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g;${SED_ENABLE_HTTPS}${SED_ENABLE_TLS}" > "${DOCKER_PATH}/data/unbound.conf"
 }
 # Update Root Hints
 function UpdateRootHints() {
