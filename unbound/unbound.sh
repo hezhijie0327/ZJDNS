e3cb577be018b29f84f987854a51143f8c634833
Zhijie He <hezhijie0327@hotmail.com>
2022-12-10 22:52:41 +0800

    unbound.sh | Version 1.0.202212102252

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -61,7 +61,7 @@ function DownloadUnboundConfiguration() {
     fi
 
     if [ "${ENABLE_TLS_UPSTREAM}" == "true" ]; then
-        sed -i "s/@5533/@5553/g;s/forward-tcp-upstream\: yes/forward-tcp-upstream\: no/g;s/forward-tls-upstream\: no/forward-tls-upstream\: yes/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/@5533/@5535/g;s/forward-tcp-upstream\: yes/forward-tcp-upstream\: no/g;s/forward-tls-upstream\: no/forward-tls-upstream\: yes/g" "${DOCKER_PATH}/data/unbound.conf"
     fi
 
     if [ "${ENABLE_UNENCRYPTED_DNS}" == "false" ]; then
