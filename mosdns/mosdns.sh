477cc58b60d2eafe4e4f6950da41a2b4bd29491c
Zhijie He <hezhijie0327@hotmail.com>
2022-12-23 07:35:56 +0800

    mosdns.sh | Version 1.0.202212230735

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -23,6 +23,8 @@ function DownloadmosDNSConfiguration() {
     ENABLE_IPV6_UPSTREAM="false"
     ENABLE_HTTP3_UPSTREAM="false"
     ENABLE_PIPELINE="false"
+
+    ENABLE_PROXY_IPV6_UPSTREAM="false"
     ENABLE_PROXY_UPSTREAM="false"
 
     ENABLE_ECS="true"
@@ -67,6 +69,9 @@ function DownloadmosDNSConfiguration() {
     fi
     if [ "${ENABLE_PIPELINE}" == "false" ]; then
         sed -i "s/enable_pipeline: true/enable_pipeline: false/g" "${DOCKER_PATH}/conf/config.yaml"
+    fi
+    if [ "${ENABLE_PROXY_IPV6_UPSTREAM}" == "true" ]; then
+        sed -i "s/#+/  /g" "${DOCKER_PATH}/conf/config.yaml"
     fi
     if [ "${ENABLE_PROXY_UPSTREAM}" == "true" ]; then
         sed -i "s/#@/  /g" "${DOCKER_PATH}/conf/config.yaml"
