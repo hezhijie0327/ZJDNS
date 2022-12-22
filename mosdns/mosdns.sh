c644f9723812a303a26a826731d2aa54d6124ae4
Zhijie He <hezhijie0327@hotmail.com>
2022-12-22 10:43:46 +0800

    mosdns.sh | Version 1.0.202212221043

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -22,7 +22,7 @@ function CleanupCurrentContainer() {
 function DownloadmosDNSConfiguration() {
     ENABLE_IPV6_UPSTREAM="false"
     ENABLE_HTTP3_UPSTREAM="false"
-    ENABLE_PIPELINE="true"
+    ENABLE_PIPELINE="false"
     ENABLE_PROXY_UPSTREAM="false"
 
     ENABLE_ECS="true"
