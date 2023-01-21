71435b5a6f936e7096f517696f27958b5025683c
Zhijie He <hezhijie0327@hotmail.com>
2023-01-21 08:21:24 +0800

    mosdns.sh | Version 1.0.202301210821

--- mosdns/mosdns.sh
+++ mosdns/mosdns.sh
@@ -79,15 +79,15 @@ function DownloadmosDNSConfiguration() {
     if [ "${ENABLE_PIPELINE}" == "false" ]; then
         sed -i "s/enable_pipeline: true/enable_pipeline: false/g" "${DOCKER_PATH}/conf/config.yaml"
     fi
-    
-    if [ "${ENABLE_PROXY_IPV6_UPSTREAM}" == "true" ] && [ "${ENABLE_IPV6_UPSTREAM}" == "true" ]; then
+
+    if [ "${ENABLE_PROXY_IPV6_UPSTREAM}" == "true" ]; then
         sed -i "s/#+/  /g" "${DOCKER_PATH}/conf/config.yaml"
     fi
-    if [ "${ENABLE_PROXY_UPSTREAM}" == "true" ] && [ "${ENABLE_REMOTE_UPSTREAM}" == "true" ]; then
+    if [ "${ENABLE_PROXY_UPSTREAM}" == "true" ]; then
         sed -i "s/#@/  /g" "${DOCKER_PATH}/conf/config.yaml"
     fi
 
-    if [ "${ENABLE_REMOTE_IPV6_UPSTREAM}" == "true" ] || [ "${ENABLE_REMOTE_UPSTREAM}" == "true" ]; then
+    if [ "${ENABLE_REMOTE_IPV6_UPSTREAM}" == "true" ]; then
         sed -i "s/#=/  /g" "${DOCKER_PATH}/conf/config.yaml"
     fi
     if [ "${ENABLE_REMOTE_UPSTREAM}" == "true" ]; then
