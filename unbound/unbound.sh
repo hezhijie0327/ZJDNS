4b7403f377a55c2486add7b7f39eaa6230939a74
Zhijie He <hezhijie0327@hotmail.com>
2023-05-05 22:28:11 +0800

    unbound.sh | Version 1.0.202305052228

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -47,60 +47,60 @@ function DownloadUnboundConfiguration() {
         CDN_PATH="source.zhijie.online"
     else
         CDN_PATH="raw.githubusercontent.com/hezhijie0327"
-    fi && if [ ! -d "${DOCKER_PATH}/data" ]; then
-        mkdir -p "${DOCKER_PATH}/data"
-    fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/unbound/unbound.conf" | sed "s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" > "${DOCKER_PATH}/data/unbound.conf"
+    fi && if [ ! -d "${DOCKER_PATH}/conf" ]; then
+        mkdir -p "${DOCKER_PATH}/conf"
+    fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/unbound/unbound.conf" | sed "s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" > "${DOCKER_PATH}/conf/unbound.conf"
 
     if [ "${ENABLE_CACHE}" == "false" ]; then
-        sed -i "s/forward-no-cache: no/forward-no-cache: yes/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/forward-no-cache: no/forward-no-cache: yes/g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_DNSSEC}" == "false" ]; then
-        sed -i "s/validator //g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/validator //g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_DNSSEC}" == "true" ] && [ "${ENABLE_DNSSEC_PERMISSIVE_MODE}" == "true" ]; then
-        sed -i "s/val-permissive-mode\: no/val-permissive-mode\: yes/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/val-permissive-mode\: no/val-permissive-mode\: yes/g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_ECS}" == "false" ]; then
-        sed -i "s/subnetcache //g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/subnetcache //g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_FORWARD}" == "true" ]; then
-        sed -i "s/#+/  /g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/#+/  /g" "${DOCKER_PATH}/conf/unbound.conf"
     else
-        sed -i "/forward-zone:/d;/#+/d" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "/forward-zone:/d;/#+/d" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_RECURSIVE_DNS}" == "true" ]; then
-        sed -i "s/forward-first: no/forward-first: yes/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/forward-first: no/forward-first: yes/g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_REDIS_CACHE}" == "false" ]; then
-        sed -i "s/cachedb //g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/cachedb //g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
 
     if [ "${ENABLE_LOGFILE}" == "true" ]; then
-        sed -i "s/##/  /g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/##/  /g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
 
     if [ "${ENABLE_RATELIMIT}" == "false" ]; then
-        sed -i "s/ratelimit\: 1000/ratelimit\: 0/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/ratelimit\: 1000/ratelimit\: 0/g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
 
     if [ "${ENABLE_TCP_UPSTREAM}" == "false" ]; then
-        sed -i "s/tcp-upstream\: yes/tcp-upstream\: no/g;s/tls-upstream\: no/tls-upstream\: no/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/tcp-upstream\: yes/tcp-upstream\: no/g;s/tls-upstream\: no/tls-upstream\: no/g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_TLS_UPSTREAM}" == "true" ]; then
-        sed -i "s/@5533/@5535/g;s/tcp-upstream\: yes/tcp-upstream\: no/g;s/tls-upstream\: no/tls-upstream\: yes/g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/@5533/@5535/g;s/tcp-upstream\: yes/tcp-upstream\: no/g;s/tls-upstream\: no/tls-upstream\: yes/g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
 
     if [ "${ENABLE_UNENCRYPTED_DNS}" == "false" ]; then
         if [ "${ENABLE_HTTPS}" == "true" ] || [ "${ENABLE_TLS}" == "true" ]; then
-            sed -i "s/    interface/#+  interface/g" "${DOCKER_PATH}/data/unbound.conf"
+            sed -i "s/    interface/#+  interface/g" "${DOCKER_PATH}/conf/unbound.conf"
         fi
     fi
 
     if [ "${ENABLE_HTTPS}" == "true" ]; then
-        sed -i "s/#@/  /g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/#@/  /g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
     if [ "${ENABLE_TLS}" == "true" ]; then
-        sed -i "s/#%/  /g" "${DOCKER_PATH}/data/unbound.conf"
+        sed -i "s/#%/  /g" "${DOCKER_PATH}/conf/unbound.conf"
     fi
 }
 # Update Root Hints
@@ -111,18 +111,28 @@ function UpdateRootHints() {
     else
         ROOT_HINTS_DOMAIN="www.internic.net"
         ROOT_HINTS_PATH="domain/named.cache"
+    fi && if [ ! -d "${DOCKER_PATH}/data" ]; then
+        mkdir -p "${DOCKER_PATH}/data"
     fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${ROOT_HINTS_DOMAIN}/${ROOT_HINTS_PATH}" > "${DOCKER_PATH}/data/root.hints"
 }
 # Create New Container
 function CreateNewContainer() {
     docker run -it --rm --entrypoint=/unbound-anchor \
-        -v ${DOCKER_PATH}/data:/usr/local/etc/unbound \
-           ${OWNER}/${REPO}:${TAG}
+        -v /etc/resolv.conf:/etc/resolv.conf:ro \
+        -v ${DOCKER_PATH}/conf:/etc/unbound/conf \
+        -v ${DOCKER_PATH}/data:/etc/unbound/data \
+           ${OWNER}/${REPO}:${TAG} \
+        -a "/etc/unbound/data/root.key" \
+        -c "/etc/unbound/data/icannbundle.pem" \
+        -f "/etc/resolv.conf" \
+        -r "/etc/unbound/data/root.hints" \
+        -R
     docker run --name ${REPO} --net host --restart=always \
         -v /docker/ssl:/usr/local/etc/ssl:ro \
-        -v ${DOCKER_PATH}/data:/usr/local/etc/unbound \
+        -v ${DOCKER_PATH}/conf:/etc/unbound/conf \
+        -v ${DOCKER_PATH}/data:/etc/unbound/data \
         -d ${OWNER}/${REPO}:${TAG} \
-        -c "/usr/local/etc/unbound/unbound.conf" \
+        -c "/etc/unbound/conf/unbound.conf" \
         -d
 }
 # Cleanup Expired Image
