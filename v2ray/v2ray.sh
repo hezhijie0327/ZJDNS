a500edc2b3a8ec573d98f8ff0fbe8fbc415a5e02
Zhijie He <hezhijie0327@hotmail.com>
2023-06-10 11:09:24 +0800

    v2ray.sh | Version 1.0.202306101109

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -9,7 +9,7 @@ DOCKER_PATH="/docker/v2ray"
 CURL_OPTION=""
 USE_CDN="true"
 
-CUSTOM_DOMAIN="" # demo.zhijie.online
+CUSTOM_SERVERNAME="" # demo.zhijie.online
 CUSTOM_UUID="" # 99235a6e-05d4-2afe-2990-5bc5cf1f5c52
 
 ENABLE_WARP="false"
@@ -37,7 +37,7 @@ function DownloadConfiguration() {
     fi
     if [ ! -d "${DOCKER_PATH}/conf" ]; then
         mkdir -p "${DOCKER_PATH}/conf"
-    fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_DOMAIN:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g;s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" "${DOCKER_PATH}/conf/config.json"
+    fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_SERVERNAME:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g;s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" "${DOCKER_PATH}/conf/config.json"
 
     if [ "${ENABLE_WARP}" == "true" ]; then
         sed -i "s/{ \"mux\": { \"concurrency\": 8, \"enable\": true }, \"protocol\": \"freedom\" }/{ \"mux\": { \"concurrency\": 8, \"enable\": true }, \"protocol\": \"socks\", \"settings\": { \"servers\": [ { \"address\": \"127.0.0.1\", \"port\": 40000 } ] } }/g" "${DOCKER_PATH}/conf/config.json"
