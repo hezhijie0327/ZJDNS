8e4781aa50b7f583d1cc9715a719d42811fbeedb
Zhijie He <hezhijie0327@hotmail.com>
2023-06-11 15:38:57 +0800

    clash.sh | Version 1.0.202306111538

--- clash/clash.sh
+++ clash/clash.sh
@@ -15,7 +15,10 @@ CUSTOM_SERVER="" # demo.zhijie.online
 CUSTOM_SERVERNAME="" # demo.zhijie.online
 CUSTOM_UUID="" # 99235a6e-05d4-2afe-2990-5bc5cf1f5c52
 
+ENABLE_SHADOWSOCKS="false"
+ENABLE_TROJAN="false"
 ENABLE_VLESS="false"
+ENABLE_VMESS="true"
 
 ## Function
 # Get Latest Image
@@ -41,8 +44,21 @@ function DownloadConfiguration() {
         mkdir -p "${DOCKER_PATH}/conf"
     fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/clash/config.yaml" > "${DOCKER_PATH}/conf/config.yaml" && sed -i "s/server: demo.zhijie.online/server: ${CUSTOM_SERVER:-demo.zhijie.online}/g;s/Host: demo.zhijie.online/Host: ${CUSTOM_SERVERNAME:-${CUSTOM_SERVER:-demo.zhijie.online}}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g" "${DOCKER_PATH}/conf/config.yaml"
 
+    if [ "${ENABLE_SHADOWSOCKS}" == "true" ]; then
+        sed -i 's/#(/  /g;s/VMESS]/SHADOWSOCKS, VMESS]/g' "${DOCKER_PATH}/conf/config.yaml"
+    fi
+    if [ "${ENABLE_TROJAN}" == "true" ]; then
+        sed -i 's/#)/  /g;s/VMESS]/TROJAN, VMESS]/g' "${DOCKER_PATH}/conf/config.yaml"
+    fi
     if [ "${ENABLE_VLESS}" == "true" ]; then
-        sed -i "s/alterId: 0, cipher: auto, //g;s/vmess/vless/g;s/443/8443/g" "${DOCKER_PATH}/conf/config.yaml"
+        sed -i 's/#</  /g;s/VMESS]/VLESS, VMESS]/g' "${DOCKER_PATH}/conf/config.yaml"
+    fi
+    if [ "${ENABLE_VMESS}" == "false" ]; then
+        sed -i 's/  - { name: VMESS/#>- { name: VMESS/g;s/VMESS]/]/g;s/, ]/]/g' "${DOCKER_PATH}/conf/config.yaml"
+    fi
+
+    if [ -f "${DOCKER_PATH}/conf/config.yaml" ]; then
+        sed -i "/#/d" "${DOCKER_PATH}/conf/config.yaml"
     fi
 
     if [ ! -d "${DOCKER_PATH}/data" ]; then
