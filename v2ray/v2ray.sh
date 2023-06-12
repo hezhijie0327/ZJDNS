b9c2485b986d5d5242763c914e0b4bf4dc9becd1
Zhijie He <hezhijie0327@hotmail.com>
2023-06-12 15:19:20 +0800

    v2ray.sh | Version 1.0.202306121519

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -12,7 +12,7 @@ USE_CDN="true"
 CUSTOM_SERVERNAME="" # demo.zhijie.online
 CUSTOM_UUID="" # 99235a6e-05d4-2afe-2990-5bc5cf1f5c52
 
-ENABLE_HTTP="false"
+ENABLE_HTTPS_PROXY="false"
 
 ENABLE_WARP="false"
 
@@ -41,7 +41,7 @@ function DownloadConfiguration() {
         mkdir -p "${DOCKER_PATH}/conf"
     fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_SERVERNAME:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g;s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" "${DOCKER_PATH}/conf/config.json"
 
-    if [ "${ENABLE_HTTP}" == "false" ]; then
+    if [ "${ENABLE_HTTPS_PROXY}" == "false" ]; then
         sed -i '/"dest": 7890/d' "${DOCKER_PATH}/conf/config.yaml"
     fi
 
