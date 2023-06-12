1e9ecdce77a7d4b969c3b82648ae74d9637ea1fd
Zhijie He <hezhijie0327@hotmail.com>
2023-06-12 15:18:27 +0800

    v2ray.sh | Version 1.0.202306121518

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -12,6 +12,8 @@ USE_CDN="true"
 CUSTOM_SERVERNAME="" # demo.zhijie.online
 CUSTOM_UUID="" # 99235a6e-05d4-2afe-2990-5bc5cf1f5c52
 
+ENABLE_HTTP="false"
+
 ENABLE_WARP="false"
 
 SSL_CERT="fullchain.cer"
@@ -38,6 +40,10 @@ function DownloadConfiguration() {
     if [ ! -d "${DOCKER_PATH}/conf" ]; then
         mkdir -p "${DOCKER_PATH}/conf"
     fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_SERVERNAME:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g;s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" "${DOCKER_PATH}/conf/config.json"
+
+    if [ "${ENABLE_HTTP}" == "false" ]; then
+        sed -i '/"dest": 7890/d' "${DOCKER_PATH}/conf/config.yaml"
+    fi
 
     if [ "${ENABLE_WARP}" == "true" ]; then
         sed -i "s/{ \"mux\": { \"concurrency\": 8, \"enable\": true }, \"protocol\": \"freedom\" }/{ \"mux\": { \"concurrency\": 8, \"enable\": true }, \"protocol\": \"socks\", \"settings\": { \"servers\": [ { \"address\": \"127.0.0.1\", \"port\": 40000 } ] } }/g" "${DOCKER_PATH}/conf/config.json"
