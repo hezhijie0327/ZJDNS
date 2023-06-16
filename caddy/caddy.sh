200417ccb8cfa44a07b3b85399d648f52a1f3c61
Zhijie He <hezhijie0327@hotmail.com>
2023-06-16 20:21:07 +0800

    caddy.sh | Version 1.0.202306162021

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -6,6 +6,12 @@ REPO="caddy"
 TAG="latest"
 DOCKER_PATH="/docker/caddy"
 
+CURL_OPTION=""
+USE_CDN="true"
+
+SSL_CERT="fullchain.cer"
+SSL_KEY="zhijie.online.key"
+
 ## Function
 # Get Latest Image
 function GetLatestImage() {
@@ -16,6 +22,17 @@ function CleanupCurrentContainer() {
     if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^${REPO}$") ]; then
         docker stop ${REPO} && docker rm ${REPO}
     fi
+}
+# Download Configuration
+function DownloadConfiguration() {
+    if [ "${USE_CDN}" == "true" ]; then
+        CDN_PATH="source.zhijie.online"
+    else
+        CDN_PATH="raw.githubusercontent.com/hezhijie0327"
+    fi
+    if [ ! -d "${DOCKER_PATH}/conf" ]; then
+        mkdir -p "${DOCKER_PATH}/conf"
+    fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/caddy/Caddyfile" > "${DOCKER_PATH}/conf/Caddyfile" && sed -i "s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" "${DOCKER_PATH}/conf/Caddyfile"
 }
 # Create New Container
 function CreateNewContainer() {
