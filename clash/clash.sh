5d1e19c5153d7c899a2656f8381160454c6f52e5
Zhijie He <hezhijie0327@hotmail.com>
2023-06-18 15:41:56 +0800

    clash.sh | Version 1.0.202306181541

--- clash/clash.sh
+++ clash/clash.sh
@@ -15,8 +15,11 @@ CUSTOM_SERVER="" # demo.zhijie.online
 CUSTOM_SERVERNAME="" # demo.zhijie.online
 CUSTOM_UUID="" # 99235a6e-05d4-2afe-2990-5bc5cf1f5c52
 
+ENABLE_SHADOWSOCKS_WSS="false"
 ENABLE_TROJAN_GRPC="false"
 ENABLE_TROJAN_WSS="false"
+ENABLE_VLESS_GRPC="false"
+ENABLE_VLESS_WSS="false"
 ENABLE_VMESS_GRPC="false"
 ENABLE_VMESS_WSS="true"
 
@@ -25,7 +28,9 @@ RUNNING_MODE="" # fallback, load-balance, url-test
 ## Function
 # Get Latest Image
 function GetLatestImage() {
-    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
+    if [ "${ENABLE_VLESS_GRPC}" == "true" ] || [ "${ENABLE_VLESS_WSS}" == "true" ]; then
+        TAG="meta"
+    fi && docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
 }
 # Cleanup Current Container
 function CleanupCurrentContainer() {
@@ -45,6 +50,10 @@ function DownloadConfiguration() {
         mkdir -p "${DOCKER_PATH}/conf"
     fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/clash/config.yaml" > "${DOCKER_PATH}/conf/config.yaml" && sed -i "s/server: demo.zhijie.online/server: ${CUSTOM_SERVER:-demo.zhijie.online}/g;s/Host: demo.zhijie.online/Host: ${CUSTOM_SERVERNAME:-${CUSTOM_SERVER:-demo.zhijie.online}}/g;s/servername: demo.zhijie.online/servername: ${CUSTOM_SERVERNAME:-${CUSTOM_SERVER:-demo.zhijie.online}}/g;s/sni: demo.zhijie.online/sni: ${CUSTOM_SERVERNAME:-${CUSTOM_SERVER:-demo.zhijie.online}}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g" "${DOCKER_PATH}/conf/config.yaml"
 
+    if [ "${ENABLE_SHADOWSOCKS_WSS}" == "true" ]; then
+        sed -i 's/#+/  /g' "${DOCKER_PATH}/conf/config.yaml"
+        PROXY_LIST="SHADOWSOCKS_WSS ${PROXY_LIST}"
+    fi
     if [ "${ENABLE_TROJAN_GRPC}" == "true" ]; then
         sed -i 's/#(/  /g' "${DOCKER_PATH}/conf/config.yaml"
         PROXY_LIST="TROJAN_GRPC ${PROXY_LIST}"
@@ -53,12 +62,20 @@ function DownloadConfiguration() {
         sed -i 's/#)/  /g' "${DOCKER_PATH}/conf/config.yaml"
         PROXY_LIST="TROJAN_WSS ${PROXY_LIST}"
     fi
+    if [ "${ENABLE_VLESS_GRPC}" == "true" ]; then
+        sed -i 's/#</  /g' "${DOCKER_PATH}/conf/config.yaml"
+        PROXY_LIST="VLESS_GRPC ${PROXY_LIST}"
+    fi
+    if [ "${ENABLE_VLESS_WSS}" == "true" ]; then
+        sed -i 's/#>/  /g' "${DOCKER_PATH}/conf/config.yaml"
+        PROXY_LIST="VLESS_WSS ${PROXY_LIST}"
+    fi
     if [ "${ENABLE_VMESS_GRPC}" == "true" ]; then
         sed -i 's/##/  /g' "${DOCKER_PATH}/conf/config.yaml"
         PROXY_LIST="VMESS_GRPC ${PROXY_LIST}"
     fi
     if [ "${ENABLE_VMESS_WSS}" == "false" ]; then
-        if [ "${ENABLE_TROJAN_GRPC}" != "false" ] || [ "${ENABLE_TROJAN_WSS}" != "false" ] || [ "${ENABLE_VMESS_GRPC}" != "false" ]; then
+        if [ "${ENABLE_SHADOWSOCKS_WSS}" != "false" ] || [ "${ENABLE_TROJAN_GRPC}" != "false" ] || [ "${ENABLE_TROJAN_WSS}" != "false" ] || [ "${ENABLE_VLESS_GRPC}" != "false" ] || [ "${ENABLE_VLESS_WSS}" != "false" ] || [ "${ENABLE_VMESS_GRPC}" != "false" ]; then
             sed -i 's/  - { name: VMESS_WSS/##- { name: VMESS_WSS/g' "${DOCKER_PATH}/conf/config.yaml"
         else
             PROXY_LIST="VMESS_WSS ${PROXY_LIST}"
