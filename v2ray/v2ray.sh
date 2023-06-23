93178576d18445618b68fe3ce09ecb5c70031343
Zhijie He <hezhijie0327@hotmail.com>
2023-06-23 19:28:00 +0800

    v2ray.sh | Version 1.0.202306231927

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -38,7 +38,7 @@ function DownloadConfiguration() {
     fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_SERVERNAME:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g" "${DOCKER_PATH}/conf/config.json"
 
     if [ "${ENABLE_WARP:-false}" == "true" ]; then
-        sed -i "s/{ \"mux\": { \"concurrency\": 8, \"enable\": true }, \"protocol\": \"freedom\" }/{ \"mux\": { \"concurrency\": 8, \"enable\": true }, \"protocol\": \"socks\", \"settings\": { \"servers\": [ { \"address\": \"127.0.0.1\", \"port\": 40000 } ] } }/g" "${DOCKER_PATH}/conf/config.json"
+        sed -i "s/{ \"protocol\": \"freedom\" }/{ \"protocol\": \"socks\", \"settings\": { \"servers\": [ { \"address\": \"127.0.0.1\", \"port\": 40000 } ] } }/g" "${DOCKER_PATH}/conf/config.json"
     fi
 
 }
