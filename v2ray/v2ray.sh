8f2325aba76ef9afb8fc068e40234ce25250c6b6
Zhijie He <hezhijie0327@hotmail.com>
2023-07-02 21:37:01 +0800

    v2ray.sh | Version 1.0.202307022136

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -10,6 +10,8 @@ CURL_OPTION=""
 DOWNLOAD_CONFIG="" # false, true
 USE_CDN="true"
 
+CNIPDB_SOURCE="" # bgp, dbip, geolite2, iana, ip2location, ipipdotnet, iptoasn, vxlink, zjdb
+
 CUSTOM_SERVERNAME="" # demo.zhijie.online
 CUSTOM_UUID="" # 99235a6e-05d4-2afe-2990-5bc5cf1f5c52
 
@@ -28,21 +30,27 @@ function CleanupCurrentContainer() {
 }
 # Download Configuration
 function DownloadConfiguration() {
+    if [ "${USE_CDN}" == "true" ]; then
+        CDN_PATH="source.zhijie.online"
+    else
+        CDN_PATH="raw.githubusercontent.com/hezhijie0327"
+    fi
+
     if [ ! -d "${DOCKER_PATH}/conf" ]; then
         mkdir -p "${DOCKER_PATH}/conf"
     fi
 
     if [ "${DOWNLOAD_CONFIG:-true}" == "true" ]; then
-        if [ "${USE_CDN}" == "true" ]; then
-            CDN_PATH="source.zhijie.online"
-        else
-            CDN_PATH="raw.githubusercontent.com/hezhijie0327"
-        fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_SERVERNAME:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g" "${DOCKER_PATH}/conf/config.json"
+        curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/v2ray/config.json" > "${DOCKER_PATH}/conf/config.json" && sed -i "s/demo.zhijie.online/${CUSTOM_SERVERNAME:-demo.zhijie.online}/g;s/99235a6e-05d4-2afe-2990-5bc5cf1f5c52/${CUSTOM_UUID:-$(uuidgen | tr 'A-Z' 'a-z')}/g" "${DOCKER_PATH}/conf/config.json"
 
         if [ "${ENABLE_WARP:-false}" == "true" ]; then
             sed -i "s/{ \"protocol\": \"freedom\" }/{ \"protocol\": \"socks\", \"settings\": { \"servers\": [ { \"address\": \"127.0.0.1\", \"port\": 40000 } ] } }/g" "${DOCKER_PATH}/conf/config.json"
         fi
     fi
+
+    if [ ! -d "${DOCKER_PATH}/data" ]; then
+        mkdir -p "${DOCKER_PATH}/data"
+    fi && curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CNIPDb/main/cnipdb_${CNIPDB_SOURCE:-geolite2}/country_ipv4_6.dat" > "${DOCKER_PATH}/data/geoip.dat"
 }
 # Create New Container
 function CreateNewContainer() {
@@ -50,6 +58,7 @@ function CreateNewContainer() {
         -v /docker/ssl:/etc/v2ray/cert:ro \
         -v /etc/resolv.conf:/etc/resolv.conf:ro \
         -v ${DOCKER_PATH}/conf:/etc/v2ray/conf \
+        -v ${DOCKER_PATH}/data:/etc/v2ray/data \
         -d ${OWNER}/${REPO}:${TAG} \
         run \
         -c /etc/v2ray/conf/config.json
