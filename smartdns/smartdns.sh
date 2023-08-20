9e87b76c31d0def516cad7a3b76691c46ba04f99
Zhijie He <hezhijie0327@hotmail.com>
2023-08-20 19:52:49 +0800

    smartdns.sh | Version 1.0.202308201952

--- smartdns/smartdns.sh
+++ smartdns/smartdns.sh
@@ -12,6 +12,8 @@ USE_CDN="true"
 
 CNIPDB_SOURCE="" # bgp, dbip, geolite2, iana, ip2location, ipipdotnet, iptoasn, vxlink, zjdb
 
+SERVER_NAME="$(hostname)" # smartdns, $(hostname)
+
 CACHE_PERSIST="" # false, true
 CACHE_SIZE="" # 4096
 
@@ -71,6 +73,10 @@ function DownloadConfiguration() {
 
     if [ "${DOWNLOAD_CONFIG:-true}" == "true" ]; then
         curl ${CURL_OPTION:--4 -s --connect-timeout 15} "https://${CDN_PATH}/CMA_DNS/main/smartdns/smartdns.conf" | sed "s/fullchain\.cer/${SSL_CERT/./\\.}/g;s/zhijie\.online\.key/${SSL_KEY/./\\.}/g" > "${DOCKER_PATH}/conf/smartdns.conf"
+
+        if [ "${SERVER_NAME}" != "" ]; then
+            sed -i "s/server-name smartdns/server-name ${SERVER_NAME}/g" "${DOCKER_PATH}/conf/smartdns.conf"
+        fi
 
         if [ "${CACHE_PERSIST}" == "false" ]; then
             sed -i "s/cache-persist yes/cache-persist no/g" "${DOCKER_PATH}/conf/smartdns.conf"
