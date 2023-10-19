f5c34b654bc5de0e47f2cd005862466ee10e0ff0
Zhijie He <hezhijie0327@hotmail.com>
2023-10-19 20:53:22 +0800

    clash.sh | Version 1.0.202310192053

--- clash/clash.sh
+++ clash/clash.sh
@@ -10,7 +10,7 @@ CURL_OPTION=""
 DOWNLOAD_CONFIG="" # false, true
 USE_CDN="true"
 
-CNIPDB_SOURCE="" # bgp, dbip, geolite2, iana, ip2location, ipipdotnet, iptoasn, vxlink, zjdb
+CNIPDB_SOURCE="" # bgp, dbip, geolite2, iana, ip2location, ipinfoio, ipipdotnet, iptoasn, vxlink, zjdb
 
 CUSTOM_SERVER="" # demo.zhijie.online
 CUSTOM_SERVERNAME="" # demo.zhijie.online
