b46b484068f3d6d9134b6a323e4328b1d79494e7
Zhijie He <hezhijie0327@hotmail.com>
2024-05-05 13:03:42 +0800

    show_stats.sh | Version 1.0.202405051303

---
+++ unbound/show_stats.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Parameter
+OWNER="hezhijie0327"
+REPO="unbound"
+TAG="latest"
+
+REDIS_OWNER="hezhijie0327"
+REDIS_REPO="redis" # redis, valkey
+REDIS_TAG="latest"
+
+DOCKER_PATH="/docker/unbound"
+
+docker run -it --rm --entrypoint=/${REDIS_REPO}-cli --net host \
+        ${REDIS_OWNER}/${REDIS_REPO}:${REDIS_TAG} \
+    -p ${REDIS_PORT:-6379} \
+    info
+
+docker run -it --rm --entrypoint=/unbound-control --net host \
+    -v ${DOCKER_PATH}/conf:/etc/unbound/conf \
+        ${OWNER}/${REPO}:${TAG} \
+    -c "/etc/unbound/conf/unbound.conf" \
+    -s "127.0.0.1@8953" \
+    stats_noreset
