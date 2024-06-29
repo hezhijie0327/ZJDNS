c21b07b45a2d0419a5807fe002ec5d5c0529b191
Zhijie He <hezhijie0327@hotmail.com>
2024-06-29 21:13:04 +0800

    show_stats.sh | Version 1.0.202406292113

--- unbound/show_stats.sh
+++ unbound/show_stats.sh
@@ -12,6 +12,7 @@ REDIS_TAG="latest"
 DOCKER_PATH="/docker/unbound"
 
 ENABLE_REDIS_CACHE="" # false, true
+FLUSHALL_REDIS_CACHE="" # false, true
 CUSTOM_REDIS_SERVER_HOST="" # 127.0.0.1
 CUSTOM_REDIS_SERVER_PORT="" # 6379
 
@@ -23,6 +24,14 @@ docker run -it --rm --entrypoint=/unbound-control --net host \
     stats_noreset
 
 if [ "${ENABLE_REDIS_CACHE:-false}" == "true" ]; then
+    if [ "${FLUSHALL_REDIS_CACHE:-false}" == "true" ]; then
+        docker run -it --rm --entrypoint=/${REDIS_REPO}-cli --net host \
+                ${REDIS_OWNER}/${REDIS_REPO}:${REDIS_TAG} \
+            -h ${CUSTOM_REDIS_SERVER_HOST:-127.0.0.1} \
+            -p ${CUSTOM_REDIS_SERVER_PORT:-6379} \
+            FLUSHALL
+    fi
+
     docker run -it --rm --entrypoint=/${REDIS_REPO}-cli --net host \
             ${REDIS_OWNER}/${REDIS_REPO}:${REDIS_TAG} \
         -h ${CUSTOM_REDIS_SERVER_HOST:-127.0.0.1} \
