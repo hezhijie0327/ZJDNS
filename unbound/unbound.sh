0059acea3d3255245e3f0e8baeedafa2224f1ee9
Zhijie He <hezhijie0327@hotmail.com>
2024-11-03 10:21:37 +0800

    unbound | Version 1.0.202411031021

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -18,6 +18,8 @@ ENABLE_DNSSEC="false"
 ENABLE_DNSSEC_PERMISSIVE_MODE="false"
 
 ENABLE_ECS="true"
+EDNS_ADDR="" # auto, 127.0.0.1, ::1
+EDNS_ADDR_TYPE="" # A, AAAA
 
 ENABLE_FORWARD="true"
 ENABLE_FORWARD_CACHE="true"
@@ -74,10 +76,6 @@ SSL_CERT="fullchain.cer"
 SSL_KEY="zhijie.online.key"
 
 ## Function
-# Get Latest Image
-function GetLatestImage() {
-    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
-}
 # Caculate Cache Size
 function CaculateCacheSize() {
     value=$(echo "$1" | sed 's/[^0-9.]//g')
@@ -96,6 +94,63 @@ function CaculateCacheSize() {
     else
         echo $calculated_bytes
     fi
+}
+# Get WAN IP
+function GetWANIP() {
+    if [ "${Type}" == "A" ]; then
+        IPv4_v6="4"
+        IP_REGEX="^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$"
+    else
+        IPv4_v6="6"
+        IP_REGEX="^(([0-9a-f]{1,4}:){7,7}[0-9a-f]{1,4}|([0-9a-f]{1,4}:){1,7}:|([0-9a-f]{1,4}:){1,6}:[0-9a-f]{1,4}|([0-9a-f]{1,4}:){1,5}(:[0-9a-f]{1,4}){1,2}|([0-9a-f]{1,4}:){1,4}(:[0-9a-f]{1,4}){1,3}|([0-9a-f]{1,4}:){1,3}(:[0-9a-f]{1,4}){1,4}|([0-9a-f]{1,4}:){1,2}(:[0-9a-f]{1,4}){1,5}|[0-9a-f]{1,4}:((:[0-9a-f]{1,4}){1,6})|:((:[0-9a-f]{1,4}){1,7}|:)|fe80:(:[0-9a-f]{0,4}){0,4}%[0-9a-z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-f]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
+    fi
+    if [ "${StaticIP:-auto}" == "auto" ]; then
+        IP_RESULT=$(curl -${IPv4_v6:-4} -s --connect-timeout 15 "https://api.cloudflare.com/cdn-cgi/trace" | grep "ip=" | sed "s/ip=//g" | grep -E "${IP_REGEX}")
+        if [ "${IP_RESULT}" == "" ]; then
+            IP_RESULT=$(curl -${IPv4_v6:-4} -s --connect-timeout 15 "https://api64.ipify.org" | grep -E "${IP_REGEX}")
+            if [ "${IP_RESULT}" == "" ]; then
+                IP_RESULT=$(dig -${IPv4_v6:-4} +short TXT @ns1.google.com o-o.myaddr.l.google.com | tr -d '"' | grep -E "${IP_REGEX}")
+                if [ "${IP_RESULT}" == "" ]; then
+                    IP_RESULT=$(dig -${IPv4_v6:-4} +short ANY @resolver1.opendns.com myip.opendns.com | grep -E "${IP_REGEX}")
+                    if [ "${IP_RESULT}" == "" ]; then
+                        echo "invalid"
+                    else
+                        echo "${IP_RESULT}"
+                    fi
+                else
+                    echo "${IP_RESULT}"
+                fi
+            else
+                echo "${IP_RESULT}"
+            fi
+        else
+            echo "${IP_RESULT}"
+        fi
+    else
+        if [ "$(echo ${StaticIP} | grep ',')" != "" ]; then
+            if [ "${Type}" == "A" ]; then
+                IP_RESULT=$(echo "${StaticIP}" | cut -d ',' -f 1 | grep -E "${IP_REGEX}")
+            else
+                IP_RESULT=$(echo "${StaticIP}" | cut -d ',' -f 2 | grep -E "${IP_REGEX}")
+            fi
+            if [ "${IP_RESULT}" == "" ]; then
+                echo "invalid"
+            else
+                echo "${IP_RESULT}"
+            fi
+        else
+            IP_RESULT=$(echo "${StaticIP}" | grep -E "${IP_REGEX}")
+            if [ "${IP_RESULT}" == "" ]; then
+                echo "invalid"
+            else
+                echo "${IP_RESULT}"
+            fi
+        fi
+    fi
+}
+# Get Latest Image
+function GetLatestImage() {
+    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
 }
 # Cleanup Current Container
 function CleanupCurrentContainer() {
@@ -143,6 +198,13 @@ function DownloadConfiguration() {
 
         if [ "${ENABLE_ECS}" == "false" ]; then
             sed -i "s/subnetcache //g" "${DOCKER_PATH}/conf/unbound.conf"
+        else
+            OVERWRITE_ADDR=$(StaticIP=${EDNS_ADDR} && Type=${EDNS_ADDR_TYPE:-A} && GetWANIP)
+            if [ "${EDNS_ADDR_TYPE}" == "A" ]; then
+                sed -i "s|client-subnet-address-override-ipv4: ''|s|client-subnet-address-override-ipv4: '${OVERWRITE_ADDR}'|g" "${DOCKER_PATH}/conf/unbound.conf"
+            else
+                sed -i "s|client-subnet-address-override-ipv6: ''|s|client-subnet-address-override-ipv6: '${OVERWRITE_ADDR}'|g" "${DOCKER_PATH}/conf/unbound.conf"
+            fi
         fi
 
         if [ "${ENABLE_FORWARD}" == "true" ]; then
